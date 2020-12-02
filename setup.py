ee8e81b52868cb959c4147c0dcc8d4b5ac7e5e38
Vahid Masoumi <vhdmsm@gmail.com>
2020-12-02 15:23:11 +0330

    bug fix

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
