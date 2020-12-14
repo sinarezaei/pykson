a75b7f2603ad7c20b9c6ccf71aaa5c70f2a6307f
Vahid Masoumi <vhdmsm@gmail.com>
2020-12-14 19:04:38 +0330

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='funniest',
-      version='0.9.9.8.5',
+      version='0.9.9.8.6',
       description='Json Serializer/Deserializer for python',
       url='https://github.com/vhdmsm/pykson',
       author='Sina Rezaei',
