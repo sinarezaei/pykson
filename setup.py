bdaa0f779504a0ce53f41606998f6e7c9ded6d94
sinarezaei1991@gmail.com <Harpalus%123>
2021-01-12 22:23:26 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(name='pykson',
                  version='0.9.9.8.3',
                  author='Sina Rezaei',
@@ -18,25 +21,6 @@ setuptools.setup(name='pykson',
                      "License :: OSI Approved :: MIT License",
                      "Operating System :: OS Independent",
                  ],
-                 install_requires=[
-                     'typing>=3.6.2',
-                     'six>=1.12.0',
-                     'pytz>=2019.3',
-                     'dateutil>=2.8.1',
-                 ],
+                 install_requires=required,
                  python_requires='>=3.6',
                  zip_safe=False)
-from setuptools import setup, find_packages
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-setup(name='funniest',
-      version='0.9.9.8.6',
-      description='Json Serializer/Deserializer for python',
-      url='https://github.com/vhdmsm/pykson',
-      author='Sina Rezaei',
-      author_email='sinarezaei1991@gmail.com',
-      license='MIT',
-      packages = find_packages(),
-      install_requires=required,
-      zip_safe=False)
